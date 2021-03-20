91916a7ab5601ec766c99d077af8fe416c2f7afc
TinaLand <huihuijiang0619@gmail.com>
2021-03-20 16:26:19 -0400

    clean up

--- MyJupiter/src/main/java/com/example/MyJupiter/serlvet/GameServlet.java
+++ MyJupiter/src/main/java/com/example/MyJupiter/serlvet/GameServlet.java
@@ -56,37 +56,3 @@ public class GameServlet extends HttpServlet {
         response.getWriter().print(mapper.writeValueAsString(game));
     }
 }
-
-//@WebServlet(name = "GameServlet",  urlPatterns = {"/game"})
-//public class GameServlet extends HttpServlet {
-////    @Override
-////    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-////        //response.getWriter().print("Hello World!");
-////        String gamename = request.getParameter("gamename");
-////        response.getWriter().print(gamename);
-////    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        //response.getWriter().print("Hello World!");
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        response.setContentType("application/json");
-//        JSONObject game = new JSONObject();
-//        game.put("name", "World of Warcraft");
-//        game.put("developer", "Blizzard Entertainment");
-//        game.put("release_time", "Feb 11, 2005");
-//        game.put("website", "https://www.worldofwarcraft.com");
-//        game.put("price", 49.99);
-//
-//        // Write game information to response body
-//        response.getWriter().print(game);
-//    }
-//
-//    @Override
-//    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        //response.getWriter().print("Hello World!");
-//    }
-//}
