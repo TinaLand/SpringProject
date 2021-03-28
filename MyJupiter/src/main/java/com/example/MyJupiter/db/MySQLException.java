c65206c1be16bf999ce92dd44847cba90cb965cb
TinaLand <huihuijiang0619@gmail.com>
2021-03-28 19:26:58 -0400

    add favorite set

---
+++ MyJupiter/src/main/java/com/example/MyJupiter/db/MySQLException.java
@@ -0,0 +1,9 @@
+package com.example.MyJupiter.db;
+
+public class MySQLException extends RuntimeException {
+    public MySQLException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+
